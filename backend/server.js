d4b902205c6f7b60f49fa42b77abcee3c1c1bf81
m-afnan2018 <m.afnan2018@gmail.com>
2024-04-05 09:17:53 +0530

    Fixed

--- backend/server.js
+++ backend/server.js
@@ -36,13 +36,13 @@ app.use("/api/messages", messageRoutes);
 
 // http://localhost:5000 => backend,frontend
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// 	// react app
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// 	});
-// }
+	// react app
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
