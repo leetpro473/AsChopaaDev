912e8c9a9b28cfe94d8fefd40cec72f62160f0cd
AsChopaaa <80022664+leetpro473@users.noreply.github.com>
2024-09-10 15:32:49 -0300

    Update SuggestedUsers.jsx

--- frontend/src/components/SuggestedUsers.jsx
+++ frontend/src/components/SuggestedUsers.jsx
@@ -32,7 +32,7 @@ const SuggestedUsers = () => {
 	return (
 		<>
 			<Text mb={4} fontWeight={"bold"}>
-				Suggested Users
+				Usuários sugeridos
 			</Text>
 			<Flex direction={"column"} gap={4}>
 				{!loading && suggestedUsers.map((user) => <SuggestedUser key={user._id} user={user} />)}
