2dbe8adcb59820bb87fbda1baf1ef9af324caaff
AsChopaaa <80022664+leetpro473@users.noreply.github.com>
2024-09-11 03:34:38 -0300

    Update UserPost.jsx

--- frontend/src/components/UserPost.jsx
+++ frontend/src/components/UserPost.jsx
@@ -1,134 +1,89 @@
-import { Avatar, Box, Button, Divider, Flex, Image, Spinner, Text } from "@chakra-ui/react";
-import Actions from "../components/Actions";
-import { useEffect } from "react";
-import Comment from "../components/Comment";
-import useGetUserProfile from "../hooks/useGetUserProfile";
-import useShowToast from "../hooks/useShowToast";
-import { useNavigate, useParams } from "react-router-dom";
-import { formatDistanceToNow } from "date-fns";
-import { useRecoilState, useRecoilValue } from "recoil";
-import userAtom from "../atoms/userAtom";
-import { DeleteIcon } from "@chakra-ui/icons";
-import postsAtom from "../atoms/postsAtom";
+import { Avatar } from "@chakra-ui/avatar";
+import { Image } from "@chakra-ui/image";
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { BsThreeDots } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import Actions from "./Actions";
+import { useState } from "react";
 
-// Lista de usernames para os quais o ícone de verificação deve ser exibido
-const VERIFIED_USERNAMES = ['luischavoso.7'];
-
-const PostPage = () => {
-	const { user, loading } = useGetUserProfile();
-	const [posts, setPosts] = useRecoilState(postsAtom);
-	const showToast = useShowToast();
-	const { pid } = useParams();
-	const currentUser = useRecoilValue(userAtom);
-	const navigate = useNavigate();
-
-	const currentPost = posts.find(post => post._id === pid); // Obtém o post atual pela ID
-
-	useEffect(() => {
-		const getPost = async () => {
-			setPosts([]);
-			try {
-				const res = await fetch(`/api/posts/${pid}`);
-				const data = await res.json();
-				if (data.error) {
-					showToast("Error", data.error, "error");
-					return;
-				}
-				setPosts([data]);
-			} catch (error) {
-				showToast("Error", error.message, "error");
-			}
-		};
-		getPost();
-	}, [showToast, pid, setPosts]);
-
-	const handleDeletePost = async () => {
-		try {
-			if (!window.confirm("Are you sure you want to delete this post?")) return;
-
-			const res = await fetch(`/api/posts/${currentPost._id}`, {
-				method: "DELETE",
-			});
-			const data = await res.json();
-			if (data.error) {
-				showToast("Error", data.error, "error");
-				return;
-			}
-			showToast("Success", "Post deleted", "success");
-			navigate(`/${user.username}`);
-		} catch (error) {
-			showToast("Error", error.message, "error");
-		}
-	};
-
-	if (!user && loading) {
-		return (
-			<Flex justifyContent={"center"}>
-				<Spinner size={"xl"} />
-			</Flex>
-		);
-	}
-
-	if (!currentPost) return null;
-
+const UserPost = ({ postImg, postTitle, likes, replies }) => {
+	const [liked, setLiked] = useState(false);
 	return (
-		<>
-			<Flex>
-				<Flex w={"full"} alignItems={"center"} gap={3}>
-					<Avatar src={user.profilePic} size={"md"} name='Mark Zuckerberg' />
-					<Flex>
-						<Text fontSize={"sm"} fontWeight={"bold"}>
-							{user.username}
+		<Link to={"/markzuckerberg/post/1"}>
+			<Flex gap={3} mb={4} py={5}>
+				<Flex flexDirection={"column"} alignItems={"center"}>
+					<Avatar size='md' name='Mark Zuckerberg' src='/zuck-avatar.png' />
+					<Box w='1px' h={"full"} bg='gray.light' my={2}></Box>
+					<Box position={"relative"} w={"full"}>
+						<Avatar
+							size='xs'
+							name='John doe'
+							src='https://bit.ly/dan-abramov'
+							position={"absolute"}
+							top={"0px"}
+							left='15px'
+							padding={"2px"}
+						/>
+						<Avatar
+							size='xs'
+							name='John doe'
+							src='https://bit.ly/sage-adebayo'
+							position={"absolute"}
+							bottom={"0px"}
+							right='-5px'
+							padding={"2px"}
+						/>
+						<Avatar
+							size='xs'
+							name='John doe'
+							src='https://bit.ly/prosper-baba'
+							position={"absolute"}
+							bottom={"0px"}
+							left='4px'
+							padding={"2px"}
+						/>
+					</Box>
+				</Flex>
+				<Flex flex={1} flexDirection={"column"} gap={2}>
+					<Flex justifyContent={"space-between"} w={"full"}>
+						<Flex w={"full"} alignItems={"center"}>
+							<Text fontSize={"sm"} fontWeight={"bold"}>
+								markzuckerberg
+							</Text>
+							<Image src='/verified.png' w={4} h={4} ml={1} />
+						</Flex>
+						<Flex gap={4} alignItems={"center"}>
+							<Text fontStyle={"sm"} color={"gray.light"}>
+								1d
+							</Text>
+							<BsThreeDots />
+						</Flex>
+					</Flex>
+
+					<Text fontSize={"sm"}>{postTitle}</Text>
+					{postImg && (
+						<Box borderRadius={6} overflow={"hidden"} border={"1px solid"} borderColor={"gray.light"}>
+							<Image src={postImg} w={"full"} />
+						</Box>
+					)}
+
+					<Flex gap={3} my={1}>
+						<Actions liked={liked} setLiked={setLiked} />
+					</Flex>
+
+					<Flex gap={2} alignItems={"center"}>
+						<Text color={"gray.light"} fontSize='sm'>
+							{replies} replies
 						</Text>
-						{/* Verifica se o username está na lista de verificados */}
-						{VERIFIED_USERNAMES.includes(user.username) && (
-							<Image src='/verified.png' w='4' h={4} ml={4} />
-						)}
+						<Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+						<Text color={"gray.light"} fontSize='sm'>
+							{likes} likes
+						</Text>
 					</Flex>
 				</Flex>
-				<Flex gap={4} alignItems={"center"}>
-					<Text fontSize={"xs"} width={36} textAlign={"right"} color={"gray.light"}>
-						{formatDistanceToNow(new Date(currentPost.createdAt))} atrás
-					</Text>
-
-					{currentUser?._id === user._id && (
-						<DeleteIcon size={20} cursor={"pointer"} onClick={handleDeletePost} />
-					)}
-				</Flex>
 			</Flex>
-
-			<Text my={3}>{currentPost.text}</Text>
-
-			{currentPost.img && (
-				<Box borderRadius={6} overflow={"hidden"} border={"1px solid"} borderColor={"gray.light"}>
-					<Image src={currentPost.img} w={"full"} />
-				</Box>
-			)}
-
-			<Flex gap={3} my={3}>
-				<Actions post={currentPost} />
-			</Flex>
-
-			<Divider my={4} />
-
-			<Flex justifyContent={"space-between"}>
-				<Flex gap={2} alignItems={"center"}>
-					<Text fontSize={"2xl"}>👋</Text>
-					<Text color={"gray.light"}>Get the app to like, reply and post.</Text>
-				</Flex>
-				<Button>Get</Button>
-			</Flex>
-
-			<Divider my={4} />
-			{currentPost.replies.map((reply) => (
-				<Comment
-					key={reply._id}
-					reply={reply}
-					lastReply={reply._id === currentPost.replies[currentPost.replies.length - 1]._id}
-				/>
-			))}
-		</>
+		</Link>
 	);
 };
 
-export default PostPage;
+export default UserPost;
