c8fc876aa25aef591e85565dd49d37a14da64e3d
AsChopaaa <80022664+leetpro473@users.noreply.github.com>
2024-09-11 03:59:08 -0300

    Update UserPost.jsx

--- frontend/src/components/UserPost.jsx
+++ frontend/src/components/UserPost.jsx
@@ -6,10 +6,13 @@ import { Link } from "react-router-dom";
 import Actions from "./Actions";
 import { useState } from "react";
 
-const UserPost = ({ postImg, postTitle, likes, replies }) => {
+// Adicione verifiedUsernames como uma propriedade
+const UserPost = ({ postImg, postTitle, likes, replies, username, verifiedUsernames }) => {
 	const [liked, setLiked] = useState(false);
+	const isVerified = verifiedUsernames.includes(username);
+
 	return (
-		<Link to={"/markzuckerberg/post/1"}>
+		<Link to={`/user/${username}/post/1`}>
 			<Flex gap={3} mb={4} py={5}>
 				<Flex flexDirection={"column"} alignItems={"center"}>
 					<Avatar size='md' name='Mark Zuckerberg' src='/zuck-avatar.png' />
@@ -48,9 +51,10 @@ const UserPost = ({ postImg, postTitle, likes, replies }) => {
 					<Flex justifyContent={"space-between"} w={"full"}>
 						<Flex w={"full"} alignItems={"center"}>
 							<Text fontSize={"sm"} fontWeight={"bold"}>
-								markzuckerberg
+								{username}
 							</Text>
-							<Image src='/verified.png' w={4} h={4} ml={1} />
+							{/* Renderize o ícone de verificação apenas se o username estiver na lista de verificados */}
+							{isVerified && <Image src='/verified.png' w={4} h={4} ml={1} />}
 						</Flex>
 						<Flex gap={4} alignItems={"center"}>
 							<Text fontStyle={"sm"} color={"gray.light"}>
