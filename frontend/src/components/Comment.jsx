5dff95eb3eab84c96d27a8c2f9504a50bf02412f
AsChopaaa <80022664+leetpro473@users.noreply.github.com>
2024-09-11 02:57:41 -0300

    Update Comment.jsx

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,22 +1,31 @@
-import { Avatar, Divider, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Divider, Flex, Text, Image } from "@chakra-ui/react";
+
+// Lista de usernames para os quais o ícone de verificação deve ser exibido
+const VERIFIED_USERNAMES = ['luischavoso.7'];
 
 const Comment = ({ reply, lastReply }) => {
-	return (
-		<>
-			<Flex gap={4} py={2} my={2} w={"full"}>
-				<Avatar src={reply.userProfilePic} size={"sm"} />
-				<Flex gap={1} w={"full"} flexDirection={"column"}>
-					<Flex w={"full"} justifyContent={"space-between"} alignItems={"center"}>
-						<Text fontSize='sm' fontWeight='bold'>
-							{reply.username}
-						</Text>
-					</Flex>
-					<Text>{reply.text}</Text>
-				</Flex>
-			</Flex>
-			{!lastReply ? <Divider /> : null}
-		</>
-	);
+    return (
+        <>
+            <Flex gap={4} py={2} my={2} w={"full"}>
+                <Avatar src={reply.userProfilePic} size={"sm"} />
+                <Flex gap={1} w={"full"} flexDirection={"column"}>
+                    <Flex w={"full"} justifyContent={"space-between"} alignItems={"center"}>
+                        <Flex alignItems={"center"}>
+                            <Text fontSize='sm' fontWeight='bold'>
+                                {reply.username}
+                            </Text>
+                            {/* Verifica se o username está na lista de verificados */}
+                            {VERIFIED_USERNAMES.includes(reply.username) && (
+                                <Image src='/verified.png' w={4} h={4} ml={1} />
+                            )}
+                        </Flex>
+                    </Flex>
+                    <Text>{reply.text}</Text>
+                </Flex>
+            </Flex>
+            {!lastReply ? <Divider /> : null}
+        </>
+    );
 };
 
 export default Comment;
