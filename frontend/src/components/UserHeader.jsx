c8a126b6487ec4a9c8b171c3bd6b73cbb023ed50
AsChopaaa <80022664+leetpro473@users.noreply.github.com>
2024-09-11 00:08:03 -0300

    Update UserHeader.jsx

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -80,7 +80,7 @@ const UserHeader = ({ user }) => {
 			)}
 			<Flex w={"full"} justifyContent={"space-between"}>
 				<Flex gap={2} alignItems={"center"}>
-					<Text color={"gray.light"}>{user.followers.length} followers</Text>
+					<Text color={"gray.light"}>{user.followers.length} seguidores</Text>
 					<Box w='1' h='1' bg={"gray.light"} borderRadius={"full"}></Box>
 					<Link color={"gray.light"}>aschopaa.com.br</Link>
 				</Flex>
