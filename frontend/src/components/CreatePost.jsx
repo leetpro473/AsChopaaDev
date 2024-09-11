917e3a91c52796db0fa95a805737a464e0fc7ae0
AsChopaaa <80022664+leetpro473@users.noreply.github.com>
2024-09-11 00:59:43 -0300

    Update CreatePost.jsx

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -141,7 +141,7 @@ const CreatePost = () => {
 
 					<ModalFooter>
 						<Button colorScheme='blue' mr={3} onClick={handleCreatePost} isLoading={loading}>
-							Post
+							Publicar
 						</Button>
 					</ModalFooter>
 				</ModalContent>
